ece34575988d12ba0e8c2db46764219cb117e5d9
CameronHyden <camh.hyden@gmail.com>
2022-05-16 17:14:49 +0100

    responsive nav logic added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,39 @@
 import "./NavBar.scss";
 import Menu from "../../assets/images/Menu.svg";
-// import useWindowSize from "../../hooks/useWindowsSize.js";
+import Search from "../../assets/images/Search.svg";
+import Upload from "../../assets/images/Upload.svg";
+import Notifications from "../../assets/images/Notifications.svg";
+import useWindowSize from "../../hooks/useWindowsSize.js";
 
 const NavBar = () => {
-  // const windowIsDesktop = useWindowSize(1024);
+  const windowIsDesktop = useWindowSize(1024);
 
-  return (
+  const mobileNav = (
     <div className="NavBar">
       <h2 className="NavBar__title">ZINESTORE</h2>
       <img className="NavBar__menu" src={Menu} alt="" />
     </div>
   );
+
+  const desktopNav = (
+    <div className="NavBarDesktop">
+      <img src={Menu} alt="" />
+      <h2 className="NavBarDesktop__title">ZINESTORE</h2>
+      <div className="NavBarDesktop__icons">
+        <img src={Search} alt="" />
+        <img src={Upload} alt="" />
+        <img src={Notifications} alt="" />
+      </div>
+    </div>
+  );
+
+  const NavBarJsx = (
+    <>
+      {!windowIsDesktop && mobileNav}
+      {windowIsDesktop && desktopNav}
+    </>
+  );
+  return NavBarJsx;
 };
 
 export default NavBar;
