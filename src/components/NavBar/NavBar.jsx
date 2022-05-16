b1e053b7e2b630d11fd225a586ce25bdaf596fd4
CameronHyden <camh.hyden@gmail.com>
2022-05-16 16:40:36 +0100

    created mobile navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,16 @@
 import "./NavBar.scss";
+import Menu from "../../assets/images/Menu.svg";
+// import useWindowSize from "../../hooks/useWindowsSize.js";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  // const windowIsDesktop = useWindowSize(1024);
+
+  return (
+    <div className="NavBar">
+      <h2 className="NavBar__title">ZINESTORE</h2>
+      <img className="NavBar__menu" src={Menu} alt="" />
+    </div>
+  );
 };
 
 export default NavBar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import Layout from "../../components/Layout/Layout";
+import NavBar from "../../components/NavBar/NavBar";
 
 const Home = () => {
   return (
     <Layout>
+      <NavBar />
       <h1>Page Heading</h1>
       <h2>Section Heading</h2>
       <h3>Panel Heading</h3>
