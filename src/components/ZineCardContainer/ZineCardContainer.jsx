9a56e7f9d08507bd450bfdc995f2e0069c56229e
CameronHyden <camh.hyden@gmail.com>
2022-05-19 12:14:24 +0100

    displayed grid in card format

--- src/components/ZineCardContainer/ZineCardContainer.jsx
+++ src/components/ZineCardContainer/ZineCardContainer.jsx
@@ -3,17 +3,22 @@ import ZineCard from "../ZineCard/ZineCard";
 import zineCardData from "../../assets/MockData";
 
 const ZineCardContainer = () => {
-  return zineCardData.map((zine) => {
-    return (
-      <ZineCard
-        key={zine.id}
-        title={zine.title}
-        Zineimage={zine.zineImage}
-        profileName={zine.name}
-        profileImage={zine.profileimage}
-      />
-    );
-  });
+  return (
+    <div className="zineCardContainer">
+      {zineCardData.map((zine) => {
+        return (
+          <ZineCard
+            key={zine.id}
+            title={zine.title}
+            Zineimage={zine.zineImage}
+            profileName={zine.name}
+            profileImage={zine.profileimage}
+          />
+        );
+      })}
+      ;
+    </div>
+  );
 };
 
 export default ZineCardContainer;
