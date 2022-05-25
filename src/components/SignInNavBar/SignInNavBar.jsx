371289b34a0d2f463e40c29cb94f109b881da367
CameronHyden <camh.hyden@gmail.com>
2022-05-25 12:04:27 +0100

    positioned signIn nav

--- src/components/SignInNavBar/SignInNavBar.jsx
+++ src/components/SignInNavBar/SignInNavBar.jsx
@@ -4,9 +4,13 @@ import Button from "../../components/Button/Button";
 const SignInNavBar = ({ zineName }) => {
   return (
     <div className="signInNavBar">
-      <h2 className="signInNavBar__title">{zineName}</h2>;
-      <Button buttonText="lOG IN" />
-      <Button buttonText="SIGN UP" />
+      <div className="signInNavBar__top">
+        <h2 className="signInNavBar__title">{zineName}</h2>;
+      </div>
+      <div className="signInNavBar__bottom">
+        <Button buttonText="lOG IN" />
+        <Button buttonText="SIGN UP" />
+      </div>
     </div>
   );
 };
